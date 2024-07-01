534a690e22f06fe55059fb637c54d8dd9f0f0d76
swachaure <62742870+swachaure@users.noreply.github.com>
2024-07-01 23:35:01 +1000

    code optimised fetchdata and added comments

--- src/App.js
+++ src/App.js
@@ -5,43 +5,28 @@ import CirclesDisplay from './CirclesDisplay';
 import FilteredColours from './FilteredColours';
 
 const App = () => {
-  const [data1, setData1] = useState([]);
+  const [data1, setData1] = useState([]);   // useState is a hook used in functional components
   const [data2, setData2] = useState([]);
-  const [loadingStatus, setLoadingStatus] = useState({ data1: false, data2: false });
+  const [loadingStatus, setLoadingStatus] = useState({ data1: 'loading', data2: 'loading' });
   const [filters, setFilters] = useState([]);
 
-  const fetchData1 = async () => {
+  const fetchData = async (url, setData, dataKey) => { //async makes a function return a promise
     try {
-      const response1 = await axios.get('/json/circles.json');
-      setData1(response1.data);
-      setLoadingStatus(prevState => ({ ...prevState, data1: 'success' }));
+      const response = await axios.get(url);   //await makes async function wait for the promise
+      setData(response.data);
+      setLoadingStatus(prevState => ({ ...prevState, [dataKey]: 'success' }));
     } catch (error) {
-      setLoadingStatus(prevState => ({ ...prevState, data1: 'error' }));
+      setLoadingStatus(prevState => ({ ...prevState, [dataKey]: 'error' }));
     }
   };
-
-  const fetchData2 = async () => {
-    try {
-      const response2 = await axios.get('/json/circles2.json');
-      setData2(response2.data);
-      setLoadingStatus(prevState => ({ ...prevState, data2: 'success' }));
-    } catch (error) {
-      setLoadingStatus(prevState => ({ ...prevState, data2: 'error' }));
-    }
-  };
-
-  useEffect(() => {
-    const fetchData = async () => {
-      setLoadingStatus({ data1: 'loading', data2: 'loading' });
-      await fetchData1();
-      await fetchData2();
+  
+  useEffect(() => {      // useEffect called after rendering the page.
+    const fetchDataAsync = async () => {
+      await fetchData('/json/circles.json', setData1, 'data1');
+      await fetchData('/json/circles2.json', setData2, 'data2');
     };
 
-    fetchData();
-
-    const interval = setInterval(fetchData, 5000); // Poll every 5 seconds
-
-    return () => clearInterval(interval); // Clear interval on component unmount
+    fetchDataAsync();
   }, []);
 
   const handleFilter = (color) => {
